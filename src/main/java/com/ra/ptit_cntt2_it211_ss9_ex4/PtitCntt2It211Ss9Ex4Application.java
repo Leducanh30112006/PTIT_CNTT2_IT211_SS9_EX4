3c033673c5f52db5e49f371ab53aa8b3f29b1be2
Leducanh30112006 <le1duc2anh3@gmail.com>
2026-05-25 22:43:12 +0700

    Initial commit

---
+++ src/main/java/com/ra/ptit_cntt2_it211_ss9_ex4/PtitCntt2It211Ss9Ex4Application.java
@@ -0,0 +1,13 @@
+package com.ra.ptit_cntt2_it211_ss9_ex4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtitCntt2It211Ss9Ex4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtitCntt2It211Ss9Ex4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ra/ptit_cntt2_it211_ss9_ex4/PtitCntt2It211Ss9Ex4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ra.ptit_cntt2_it211_ss9_ex4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtitCntt2It211Ss9Ex4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
